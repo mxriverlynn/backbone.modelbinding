d1c392a39cab99b2752f3e3a8bd56ce440baed41
Derick Bailey <derickbailey@gmail.com>
2011-07-24 12:44:45 -0500

    DRYing up the bi-directional binding code

--- backbone.modelbinding.js
+++ backbone.modelbinding.js
@@ -1,29 +1,42 @@
 (function(){
 
+  function bidirectionalBinding(field, element, model){
+    // bind the model changes to the form elements
+    model.bind("change:" + field, function(changed_model, val){
+      element.val(val);
+    });
+
+    // bind the form changes to the model
+    element.bind("change", function(ev){
+      data = {};
+      data[field] = $(ev.target).val();
+      model.set(data);
+    });
+
+    // set the default value on the form, from the model
+    var attr_value = model.get(field);
+    if (attr_value) {
+      element.val(attr_value);
+    }
+  }
+
+  function handleConventionBindings(view, model){
+    this.$("input").each(function(index){
+      var element = $(this);
+      var field = element.attr('id');
+
+      bidirectionalBinding(field, element, model);
+    });
+  }
+
   function handleFormBindings(view, model){
     _.each(view.formBindings, function(field, selector_event){
       var selector_parts = selector_event.split(" ");
       var e = selector_parts[0];
       var selector = selector_parts[1];
+      var element = $(selector);
 
-      // bind the model changes to the form elements
-      model.bind("change:" + field, function(changed_model, val){
-        $(selector).val(val);
-      });
-
-      // bind the form changes to the model
-      $(selector).bind(e, function(ev){
-        data = {};
-        data[field] = $(ev.target).val();
-        model.set(data);
-      });
-
-      // set the default value on the form, from the model
-      var attr_value = model.get(field);
-      if (attr_value) {
-        $(selector).val(attr_value);
-      }
-
+      bidirectionalBinding(field, element, model);
     }, view);
   }
 
@@ -34,32 +47,6 @@
         $(htmlElement).html(val);
       });
     }, view);
-  }
-
-  function handleConventionBindings(view, model){
-    this.$("input").each(function(index){
-      var element = $(this);
-      var field = element.attr('id');
-
-      // bind the model changes to the form elements
-      model.bind("change:" + field, function(changed_model, val){
-        element.val(val);
-      });
-
-      // bind the form changes to the model
-      element.bind("change", function(ev){
-        data = {};
-        data[field] = $(ev.target).val();
-        model.set(data);
-      });
-
-      // set the default value on the form, from the model
-      var attr_value = model.get(field);
-      if (attr_value) {
-        element.val(attr_value);
-      }
-
-    });
   }
 
   this.ModelBinding = function(){
