89399c1869c01aa2fb2eb27c4baf2e6871581c20
Derick Bailey <derickbailey@gmail.com>
2011-07-25 22:12:19 -0500

    encapsulating checkbox and radio button code better

--- backbone.modelbinding.js
+++ backbone.modelbinding.js
@@ -15,9 +15,9 @@ Backbone.ModelBinding = (function(){
     for (var conventionName in conventions){
       if (conventions.hasOwnProperty(conventionName)){
         var conventionElement = conventions[conventionName];
-        var conventionClass = conventionElement.convention;
+        var handler = conventionElement.handler;
         var conventionSelector = conventionElement.selector;
-        conventionClass.bind(conventionSelector, view, model);
+        handler.bind(conventionSelector, view, model);
       }
     }
   }
@@ -64,35 +64,102 @@ Backbone.ModelBinding.FormFieldConventions = (function(){
 
   var RadioGroup = {
     bind: function(selector, view, model){
+      var self = this;
       var foundElements = [];
       view.$(selector).each(function(index){
         var element = view.$(this);
         var group_name = element.attr('name');
         if (!foundElements[group_name]) {
           foundElements[group_name] = true;
-          Backbone.ModelBinding.HelperMethods.bidirectionalRadioGroupBinding.call(view, group_name, model);
+          self.bidirectionalRadioGroupBinding.call(view, group_name, model);
         }
       });
+    },
+
+    bidirectionalRadioGroupBinding: function(group_name, model){
+      var self = this;
+
+      // bind the model changes to the form elements
+      model.bind("change:" + group_name, function(changed_model, val){
+        var value_selector = "input[type=radio][name=" + group_name + "][value=" + val + "]";
+        self.$(value_selector).attr("checked", "checked");
+      });
+
+      // bind the form changes to the model
+      var group_selector = "input[type=radio][name=" + group_name + "]";
+      self.$(group_selector).bind("change", function(ev){
+        var element = self.$(ev.currentTarget);
+        if (element.attr("checked")){
+          data = {};
+          data[group_name] = element.val();
+          model.set(data);
+        }
+      });
+
+      // set the default value on the form, from the model
+      var attr_value = model.get(group_name);
+      if (attr_value) {
+        var value_selector = "input[type=radio][name=" + group_name + "][value=" + attr_value + "]";
+        self.$(value_selector).attr("checked", "checked");
+      }
     }
+
   };
 
   var Checkbox = {
     bind: function(selector, view, model){
+      var self = this;
       view.$(selector).each(function(index){
         var element = view.$(this);
         var field = element.attr('id');
-        Backbone.ModelBinding.HelperMethods.bidirectionalCheckboxBinding.call(view, field, element, model);
+        self.bidirectionalCheckboxBinding.call(view, field, element, model);
       });
+    },
+
+    bidirectionalCheckboxBinding: function(attribute_name, element, model){
+      var self = this;
+
+      // bind the model changes to the form elements
+      model.bind("change:" + attribute_name, function(changed_model, val){
+        if (val){
+          element.attr("checked", "checked");
+        }
+        else{
+          element.removeAttr("checked");
+        }
+      });
+
+      // bind the form changes to the model
+      element.bind("change", function(ev){
+        data = {};
+        var changedElement = self.$(ev.target);
+        var checked = changedElement.attr("checked")? true : false;
+        data[attribute_name] = checked;
+        model.set(data);
+      });
+
+      // set the default value on the form, from the model
+      var attr_exists = model.attributes.hasOwnProperty(attribute_name);
+      if (attr_exists) {
+        var attr_value = model.get(attribute_name);
+        if (attr_value){
+          element.attr("checked", "checked");
+        }
+        else{
+          element.removeAttr("checked");
+        }
+      }
     }
+
   };
 
   return {
-    text: {selector: "input[type=text]", convention: StandardInput}, 
-    password: {selector: "input[type=password]", convention: StandardInput},
-    radioGroup: {selector: "input[type=radio]", convention: RadioGroup},
-    checkbox: {selector: "input[type=checkbox]", convention: Checkbox},
-    select: {selector: "select", convention: StandardInput},
-    textarea: {selector: "textarea", convention: StandardInput},
+    text: {selector: "input[type=text]", handler: StandardInput}, 
+    password: {selector: "input[type=password]", handler: StandardInput},
+    radioGroup: {selector: "input[type=radio]", handler: RadioGroup},
+    checkbox: {selector: "input[type=checkbox]", handler: Checkbox},
+    select: {selector: "select", handler: StandardInput},
+    textarea: {selector: "textarea", handler: StandardInput},
   }
 })();
 
@@ -125,72 +192,7 @@ Backbone.ModelBinding.HelperMethods = (function(){
     }
   }
 
-  function bidirectionalCheckboxBinding(attribute_name, element, model){
-    var self = this;
-
-    // bind the model changes to the form elements
-    model.bind("change:" + attribute_name, function(changed_model, val){
-      if (val){
-        element.attr("checked", "checked");
-      }
-      else{
-        element.removeAttr("checked");
-      }
-    });
-
-    // bind the form changes to the model
-    element.bind("change", function(ev){
-      data = {};
-      var changedElement = self.$(ev.target);
-      var checked = changedElement.attr("checked")? true : false;
-      data[attribute_name] = checked;
-      model.set(data);
-    });
-
-    // set the default value on the form, from the model
-    var attr_exists = model.attributes.hasOwnProperty(attribute_name);
-    if (attr_exists) {
-      var attr_value = model.get(attribute_name);
-      if (attr_value){
-        element.attr("checked", "checked");
-      }
-      else{
-        element.removeAttr("checked");
-      }
-    }
-  }
-
-  function bidirectionalRadioGroupBinding(group_name, model){
-    var self = this;
-
-    // bind the model changes to the form elements
-    model.bind("change:" + group_name, function(changed_model, val){
-      var value_selector = "input[type=radio][name=" + group_name + "][value=" + val + "]";
-      self.$(value_selector).attr("checked", "checked");
-    });
-
-    // bind the form changes to the model
-    var group_selector = "input[type=radio][name=" + group_name + "]";
-    self.$(group_selector).bind("change", function(ev){
-      var element = self.$(ev.currentTarget);
-      if (element.attr("checked")){
-        data = {};
-        data[group_name] = element.val();
-        model.set(data);
-      }
-    });
-
-    // set the default value on the form, from the model
-    var attr_value = model.get(group_name);
-    if (attr_value) {
-      var value_selector = "input[type=radio][name=" + group_name + "][value=" + attr_value + "]";
-      self.$(value_selector).attr("checked", "checked");
-    }
-  }
-
   return {
     bidirectionalBinding: bidirectionalBinding,
-    bidirectionalRadioGroupBinding: bidirectionalRadioGroupBinding,
-    bidirectionalCheckboxBinding: bidirectionalCheckboxBinding
   }
 })();
