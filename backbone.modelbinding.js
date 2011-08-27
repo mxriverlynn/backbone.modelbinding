2de8d0b5b6f685f4895875d0c2e67df423eb5cca
Derick Bailey <derickbailey@gmail.com>
2011-08-27 12:09:07 -0500

    bumped version to v0.3.6

--- backbone.modelbinding.js
+++ backbone.modelbinding.js
@@ -1,4 +1,4 @@
-// Backbone.ModelBinding v0.3.5
+// Backbone.ModelBinding v0.3.6
 //
 // Copyright (C)2011 Derick Bailey, Muted Solutions, LLC
 // Distributed Under MIT Liscene
@@ -37,7 +37,7 @@ Backbone.ModelBinding = (function(){
   }
 
   return {
-    version: "0.3.5",
+    version: "0.3.6",
 
     bind: function(view, options){
       Backbone.ModelBinding.Configuration.configureBindingAttributes(options);
